c23a9ffce6dd63a40efac2d525a80cc7032264a7
jtay78868 <128756190+jtay78868@users.noreply.github.com>
2023-03-28 15:23:28 -0500

    Add files via upload

---
+++ patient.sql
@@ -0,0 +1,11 @@
+SELECT PatientID, Disease, Dispensed_Drug
+FROM Patient
+JOIN Drug
+ON Patient.PatientID = Drug.PatientID
+
+
+
+SELECT CASE
+	WHEN drug.PatientID > 2 THEN ‘disease.PatientID’
+	ELSE ‘PatientID’
+END
